2eda32fe6b8dd5f0f0a38adc65302c8481d77978
Silvia Bastea <silvia.bastea@ctbav.ro>
2018-09-05 15:38:56 +0300

    display temperature received from raspberry on UI

--- temperature-computer/src/main/java/com/siemens/ct/bam/GUI/TemperatureMeasurementUI.java
+++ temperature-computer/src/main/java/com/siemens/ct/bam/GUI/TemperatureMeasurementUI.java
@@ -1,7 +1,9 @@
 package com.siemens.ct.bam.GUI;
 
-import com.siemens.ct.bam.broker.DataService;
+import com.google.gson.Gson;
+import com.siemens.ct.bam.commons.MessageConsumerFromBrokerService;
 import com.siemens.ct.bam.commons.broker.BrokerService;
+import com.siemens.ct.bam.commons.constsnts.Constants;
 import com.siemens.ct.bam.commons.models.RequestType;
 import com.siemens.ct.bam.commons.models.TemperatureMeasurement;
 
@@ -9,21 +11,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.concurrent.TimeUnit;
 
 public class TemperatureMeasurementUI extends JDialog {
 
-    private JPanel measurementPanel;
+    private JPanel MeasurementPanel;
     private JComboBox cityNames;
     private JButton buttonGetTemp;
     private JTextArea temperatureMessages;
-
-    DataService dataService = new DataService();
+    private JTextArea avgMessages;
+    private JTextField measurementIntervalField;
+    private JTextField reportIntervalField;
+    private JButton buttonOnOffAvg;
 
     public TemperatureMeasurementUI() {
         this.setMinimumSize(new Dimension(500, 500));
-        setContentPane(measurementPanel);
+        setContentPane(MeasurementPanel);
         setModal(true);
+        temperatureMessages.setPreferredSize(new Dimension(20, 20));
+
+
         cityNames.setModel(new DefaultComboBoxModel(UIUtil.getCityNames().toArray()));
 
 
@@ -34,19 +40,33 @@ public class TemperatureMeasurementUI extends JDialog {
                 String cityNameFromComboBox = (String) cityNames.getSelectedItem();
                 RequestType request = new RequestType(RequestType.TEMPERATURE_REQUEST, cityNameFromComboBox);
 
-                dataService.sendRequest(request);
+                String message = new Gson().toJson(request);
+                BrokerService.getInstance().sendMessage(message, Constants.COMMAND);
+                temperatureMessages.setText("");
             }
         });
-/*
-        temperatureMessages.setPreferredSize(new Dimension(20,20));
-        TemperatureMeasurement temperatureMeasurement = dataService.receiveTemperatureMeasurement();
-        if (temperatureMeasurement == null)
-            temperatureMessages.setText("Nothing yet");
-        else {
-            temperatureMessages.setText(temperatureMeasurement.toString());
-        }
-*/
+
+
+        BrokerService.getInstance().receiveMessage(Constants.TEMPERATURE_MEASUREMENT, new MessageConsumerFromBrokerService() {
+            @Override
+            public void onNewMessage(String s) {
+
+//                try {
+//                    TimeUnit.SECONDS.sleep(5);
+//                } catch (InterruptedException e1) {
+//                    e1.printStackTrace();
+//                }
+                if (s != null) {
+                    TemperatureMeasurement temperatureMeasurement = new Gson().fromJson(s, TemperatureMeasurement.class);
+                    temperatureMessages.setText(temperatureMeasurement.toString());
+                } else
+                    temperatureMessages.setText("Something don't work. Please check the raspberry.");
+            }
+        });
+
+
     }
+
 
     @Override
     public void dispose() {
